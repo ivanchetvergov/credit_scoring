e413019905f682ed2b1f4ed2e2a84c3ba0086f9c
ivanchetvergov <ivar5165885@gmail.com>
2025-11-13 22:24:11 +0300

    feat: lightgbm patch, but CatBoost and XGBoost dont't work

--- src/pipelines/lightgbm_pipeline.py
+++ src/pipelines/lightgbm_pipeline.py
@@ -3,11 +3,9 @@
 from lightgbm import LGBMClassifier
 from sklearn.base import BaseEstimator
 from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.compose import ColumnTransformer
 
 from src.pipelines.base_pipeline import BasePipelineBuilder
-from src.config import NUMERICAL_FEATURES, CATEGORICAL_FEATURES, BIN_CATEGORICAL_FEATURES
+from src.pipelines.preprocessor import get_model_specific_pipeline
 
 class LightGBMPipelineBuilder(BasePipelineBuilder):
     """
@@ -15,53 +13,15 @@ class LightGBMPipelineBuilder(BasePipelineBuilder):
     Использует минимальный препроцессинг (только импутация).
     Логика FE берется из BasePipelineBuilder.
     """
-
-    @staticmethod
-    def _get_lgbm_preprocessor_pipe(self) -> Pipeline:
-        """
-        создает Pipeline, содержащий ColumnTransformer для минимальной предобработки LightGBM.
-        выполняет только импутацию.
-        """
-
-        # 1. пайплайн для числовых (только импутация медианой)
-        numerical_lgbm_pipe = Pipeline(
-            steps=[('imputer', SimpleImputer(strategy='median'))]
-        )
-
-        # 2. пайплайн для категориальных (только импутация константой 'missing')
-        categorical_lgbm_pipe = Pipeline(
-            steps=[('imputer', SimpleImputer(strategy='constant', fill_value='missing'))]
-        )
-
-        cat_and_bin_features = CATEGORICAL_FEATURES + BIN_CATEGORICAL_FEATURES
-
-        # ColumnTransformer
-        lgbm_preprocessor = ColumnTransformer(
-            transformers=[
-                ('num', numerical_lgbm_pipe, NUMERICAL_FEATURES),
-                ('cat_and_bin', categorical_lgbm_pipe, cat_and_bin_features),
-            ],
-            remainder='drop',
-            verbose_feature_names_out=False
-        )
-
-        # Инкапсулируем ColumnTransformer в Pipeline
-        return Pipeline(steps=[('lgbm_imputer_ct', lgbm_preprocessor)])
-
     def _get_preprocessor(self, feature_engineering: bool = True) -> Pipeline:
         """
         собирает полный пайплайн препроцессинга: FE (если нужно) -> LGBM-препроцессор.
         """
-        steps = []
+        return get_model_specific_pipeline(
+            model_name='lightgbm',
+            include_feature_engineering=feature_engineering
+        )
 
-        if feature_engineering:
-            # 1. общие шаги feature engineering (из BasePipelineBuilder)
-            steps.append(('feature_engineering', self._get_feature_engineering_pipeline()))
-
-        # 2. специфичный для LightGBM препроцессор (только импутация)
-        steps.append(('lgbm_preprocessor', self._get_lgbm_preprocessor_pipe()))
-
-        return Pipeline(steps=steps)
 
     def _get_model(self) -> BaseEstimator:
         """
